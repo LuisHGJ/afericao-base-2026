224490392842f2b7b909838f5de865c8b6fe07eb
Luis Henrique <110108496+LuisHGJ@users.noreply.github.com>
2026-04-15 19:26:27 +0000

    o

--- src/main/java/application/config/OpenApiConfig.java
+++ src/main/java/application/config/OpenApiConfig.java
@@ -3,13 +3,33 @@ package application.config;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.security.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequiriment;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @Configuration
 @OpenAPIDefinition(
+    info = @Info(
+        title = "API sensores",
+        version = "v1",
+        description = "API para gerenciamento de dados recebidos de sensores",
+        contact = @Contact(
+            name = "Luis",
+            email = "luis@gmail.com"
+        )
+    ),
     servers = {
         @Server(url="/", description="Servidor Atual"),
         @Server(url="https://api-teste.com", description="Servidor de Teste")
-    }
+    },
+    security = @SecurityRequirement(name = "bearerAuth")
+)
+
+@SecurityScheme(
+    name ="bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat "JWT"
 )
 
 public class OpenApiConfig {
